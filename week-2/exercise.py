49caf31ed85e3a00439ac9014456e116a7492774
Mahmut Ozge Karakaya <mokarakaya@gmail.com>
2018-01-02 23:12:25 +0100

    init

---
+++ week-2/exercise.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.50})
+purchase_2 = pd.Series({'Name': 'Kevin',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.50})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.00})
+
+df = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+
+
+
+#print df['Cost'] > 3.00
+#df['Cost'] *= 0.8
+#print df
